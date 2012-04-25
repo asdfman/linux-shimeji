3876983539462d221b1cc0a77d772168cbf5bd0d
asdf man <asdfman@lavabit.com>
2012-04-25 16:06:23 +0300

    testing

--- src/com/group_finity/mascot/Mascot.java
+++ src/com/group_finity/mascot/Mascot.java
@@ -156,10 +156,10 @@ public class Mascot {
 					getWindow().asJWindow().setVisible(false);
 				}
 			}
-			if (time == 2 || time == 100) {
+			if (time == 2 || time == 5) {
 				getWindow().setToDock(environment.getDockValue());
 			}
-			if (time == 3) {
+			if (time == 3 || time == 6) {
 				getWindow().asJWindow().setVisible(false);
 			}
 		}
