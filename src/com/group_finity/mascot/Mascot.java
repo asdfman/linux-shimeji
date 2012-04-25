1f4d7332ae2c7fc1507900f0ef035ede05436f97
asdf man <asdfman@lavabit.com>
2012-04-25 16:32:20 +0300

    it just werks

--- src/com/group_finity/mascot/Mascot.java
+++ src/com/group_finity/mascot/Mascot.java
@@ -156,10 +156,10 @@ public class Mascot {
 					getWindow().asJWindow().setVisible(false);
 				}
 			}
-			if (time == 2 || time == 50) {
+			if (time == 2 || time == 20 || time == 100) {
 				getWindow().setToDock(environment.getDockValue());
 			}
-			if (time == 3 || time == 50) {
+			if (time == 3 || time == 20 || time == 100) {
 				getWindow().asJWindow().setVisible(false);
 			}
 		}
